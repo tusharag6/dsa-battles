0e0e2ce127eaee444930d6311075807003124517
Tushar Agrawal <tushar.code.inbox@gmail.com>
2024-09-13 12:45:45 +0530

    finished pnpm workspace setup

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@dsa-battle/shared-ui";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <Button onClick={() => console.log("clicked")}>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
